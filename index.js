510ceb317611ee676a7b390d232c65417292f77a
pdwarkanath <dwarkanath.prabhu@gmail.com>
2022-06-11 20:51:50 +0530

    refactored controller

--- index.js
+++ index.js
@@ -3,28 +3,10 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
-
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  console.log(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const blogRouter = require('./controller/blog')
+app.use('/api/blogs', blogRouter)
 
 const PORT = config.PORT || 3003
 app.listen(PORT, () => {
