dc8fb9bbff3561d7ddda93f687022660d1fea80e
pdwarkanath <dwarkanath.prabhu@gmail.com>
2022-06-25 17:18:29 +0530

    ex 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,9 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => blogs.reduce((acc, blog) => acc + blog.likes, 0)
   
-  module.exports = {
-    dummy
-  }
+module.exports = {
+    dummy, totalLikes
+}
