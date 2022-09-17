edebdf8bd527981d8c5bbcb2de6c07aaf51d2ba3
pdwarkanath <dwarkanath.prabhu@gmail.com>
2022-09-17 16:40:12 +0530

    removed login requirement for get

--- controller/blog.js
+++ controller/blog.js
@@ -26,28 +26,36 @@ blogRouter.delete('/:id', async (request, response) => {
 
 blogRouter.patch('/:id', async (request, response) => {
   const user = request.user
-  const blog = await Blog.findById(request.params.id)
-  if (blog.user.equals(user._id)) {
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, request.body)
-    updatedBlog ? response.status(201).json(updatedBlog) : response.status(400).end()
+  if (user) {
+    const blog = await Blog.findById(request.params.id)
+    if (blog.user.equals(user._id)) {
+      const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, request.body)
+      updatedBlog ? response.status(201).json(updatedBlog) : response.status(400).end()
+    } else {
+      response.status(401).json({ error: 'invalid user' });
+    }
   } else {
-    response.status(401).json({ error: 'invalid user' });
+    response.status(400).send()
   }
 })
   
   
 blogRouter.post('/', async (request, response) => {
   const user = request.user
-  const blog = new Blog(request.body)
-  blog.user = user._id
-  const validation = blog.validateSync()
-  if (validation) { 
-    response.status(400).send()
+  if (user) {
+    const blog = new Blog(request.body)
+    blog.user = user._id
+    const validation = blog.validateSync()
+    if (validation) { 
+      response.status(400).send()
+    } else {
+      const savedBlog = await blog.save()
+      user.blogs = user.blogs.concat(savedBlog._id)
+      await user.save()
+      response.status(201).json(savedBlog)
+    }
   } else {
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-    response.status(201).json(savedBlog)
+    response.status(400).send()
   }
 })
 
--- utils/middleware.js
+++ utils/middleware.js
@@ -10,11 +10,13 @@ const tokenExtractor = (request, response, next) => {
 
 
 const userExtractor = async (request, response, next) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
+  if (request.token) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    request.user = await User.findById(decodedToken.id)
   }
-  request.user = await User.findById(decodedToken.id)
   next()
 }
 
